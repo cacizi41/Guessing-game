040d9d9b9d6e0561f332062935af59e4bfe23ce8
Wenjing <mac@Gingers-MacBook-Pro.local>
2015-09-30 15:25:57 -0700

    add in error correction

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
-var question1 = prompt("Is your favorite movie Pulp fiction?");
+var question1 = prompt('Is your favorite movie Pulp fiction?');
 
-if(question1 === 'yes'){
+if(question1.toUpperCase() === 'YES'|| question1.toUpperCase() === 'Y') {
 	alert("great! You got great taste!");
-} else { alert("I wish you would love it!")}
+} else { 
+	alert("I wish you would love it!");
+}
 
 var question2 = prompt("Do you like Arcade fire?");
 
-if(question1 === 'yes'){
+if(question2.toUpperCase() === 'YES'|| question2.toUpperCase()==='Y'){
 	alert("we can be friends then!")
 } else {alert("I would still be your friend.")}
 
 var question3 = prompt("Do you want to ride bike to CF?")
 
-if(question1 === 'yes'){
+if(question3.toUpperCase() === 'YES'|| question3.toUpperCase()==='Y'){
 	alert("Great workout! You can shower there.")
 } else {alert("Traffic is horrible those days")}
