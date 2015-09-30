2ed26714889093de33e687fab60f9afdd904460d
Wenjing <mac@Gingers-MacBook-Pro.local>
2015-09-30 14:46:16 -0700

    Rspond to the user using if/else and alerts

--- game.js
+++ game.js
@@ -1,34 +1,17 @@
 var question1 = prompt("Is your favorite movie Pulp fiction?");
+
+if(question1 === 'yes'){
+	alert("great! You got great taste!");
+} else { alert("I wish you would love it!")}
+
 var question2 = prompt("Do you like Arcade fire?");
+
+if(question1 === 'yes'){
+	alert("we can be friends then!")
+} else {alert("I would still be your friend.")}
+
 var question3 = prompt("Do you want to ride bike to CF?")
 
-if(question1 = yes){
-	alert('great!')
-} else {'suck to be you'}
-
-
-
-
-
-
-console.log("Q1 response: " + question1);
-console.log("Q2 response: " + question2);
-console.log("Q3 response: " + question3);
-
-alert('your answer to question 1 was' + question1)
-alert('your answer to question 2 was' + question2)
-alert('your answer to question 3 was' + question3)
-
-
-
-var question1 = prompt("Is your favorite movie Pulp fiction?");
-
-if(question1.toUpperCase() = "yes" || condition2){
-	alert('great!')
-} else {'suck to be you'}
-
-console.log("Q1 response" + question1.toUpperCase());
-console.log()
-
-//changes....
-//summary
+if(question1 === 'yes'){
+	alert("Great workout! You can shower there.")
+} else {alert("Traffic is horrible those days")}
