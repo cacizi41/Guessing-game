3b65dbf9e81453be1d5a16e9df493a77dc455edd
Wenjing <mac@Gingers-MacBook-Pro.local>
2015-09-30 13:54:35 -0700

    initial update

---
+++ game.js
@@ -0,0 +1,34 @@
+var question1 = prompt("Is your favorite movie Pulp fiction?");
+var question2 = prompt("Do you like Arcade fire?");
+var question3 = prompt("Do you want to ride bike to CF?")
+
+if(question1 = yes){
+	alert('great!')
+} else {'suck to be you'}
+
+
+
+
+
+
+console.log("Q1 response: " + question1);
+console.log("Q2 response: " + question2);
+console.log("Q3 response: " + question3);
+
+alert('your answer to question 1 was' + question1)
+alert('your answer to question 2 was' + question2)
+alert('your answer to question 3 was' + question3)
+
+
+
+var question1 = prompt("Is your favorite movie Pulp fiction?");
+
+if(question1.toUpperCase() = "yes" || condition2){
+	alert('great!')
+} else {'suck to be you'}
+
+console.log("Q1 response" + question1.toUpperCase());
+console.log()
+
+//changes....
+//summary
